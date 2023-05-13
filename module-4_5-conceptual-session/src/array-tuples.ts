655d959a1b713b49ce78938402805cb65acd0f4d
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-13 21:27:30 +0600

    e-4_5

---
+++ module-4_5-conceptual-session/src/array-tuples.ts
@@ -0,0 +1,7 @@
+// array
+const names: string[] = ["Abul", "Babul", "Cabul"];
+const numbers: number[] = [1, 2, 3, 4, 5];
+const heroInfo: (string | boolean | number)[] = ["Abul", 1, "Actor", true];
+
+// tuple
+const heroTuple: [string, boolean, number] = ["Abul", false, 3];
