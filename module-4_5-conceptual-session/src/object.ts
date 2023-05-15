1d5b782e1756744fbf37f61e17f8e36bd8904599
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-15 21:37:34 +0600

    feat: conceptual session 4_5 added

--- module-4_5-conceptual-session/src/object.ts
+++ module-4_5-conceptual-session/src/object.ts
@@ -1,16 +1,16 @@
-const user: {
-  company: "PH"; //literal type (You can not change this value)
-  readonly name: string;
-  age: number;
-  isMarried: boolean;
-  wife?: string; // if there is no wife it will not be a problem
-} = {
-  company: "PH",
-  name: "Abul Mia",
-  age: 52,
-  isMarried: true,
-  wife: "Bibi Sokina",
-};
+// const user: {
+//   company: "PH"; //literal type (You can not change this value)
+//   readonly name: string;
+//   age: number;
+//   isMarried: boolean;
+//   wife?: string; // if there is no wife it will not be a problem
+// } = {
+//   company: "PH",
+//   name: "Abul Mia",
+//   age: 52,
+//   isMarried: true,
+//   wife: "Bibi Sokina",
+// };
 
-// user.name = "Babul Mia" // You cannot change it (read-only)
-user.wife = "Hadija banu";
+// // user.name = "Babul Mia" // You cannot change it (read-only)
+// user.wife = "Hadija banu";
--- module-4_5-conceptual-session/src/spread-rest.ts
+++ module-4_5-conceptual-session/src/spread-rest.ts
@@ -1,16 +1,16 @@
-// Spread Operator
-const heroArr1: string[] = ["Abul", "Babul", "Cabul"];
-const heroArr2: string[] = ["Dabul", "Ebul", "Febul"];
+// // Spread Operator
+// const heroArr1: string[] = ["Abul", "Babul", "Cabul"];
+// const heroArr2: string[] = ["Dabul", "Ebul", "Febul"];
 
-heroArr1.push(...heroArr2);
-console.log(heroArr1);
+// heroArr1.push(...heroArr2);
+// console.log(heroArr1);
 
-// Rest Operator
-const heroScore = (...score: number[]) => {
-  let sum: number = 0;
-  score.map((n) => {
-    sum += n;
-  });
-  return sum;
-};
-console.log(heroScore(1, 2, 3, 4, 5));
+// // Rest Operator
+// const heroScore = (...score: number[]) => {
+//   let sum: number = 0;
+//   score.map((n) => {
+//     sum += n;
+//   });
+//   return sum;
+// };
+// console.log(heroScore(1, 2, 3, 4, 5));
