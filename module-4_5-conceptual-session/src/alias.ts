3ad2c8444dfe08e3bd7dc7e4b2d01394c2d81ac9
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-16 14:29:31 +0600

    union-intersection.ts file added

--- module-4_5-conceptual-session/src/alias.ts
+++ module-4_5-conceptual-session/src/alias.ts
@@ -1,8 +1,10 @@
+/* ===== type will work single value/object/array/... ===== */
 type HereIncomeType = number | string;
 
 const hereIncome1: HereIncomeType = 15;
 const hereIncome2: HereIncomeType = "15";
 
+/* ===== Interface will work only for objects ===== */
 interface IHeroType {
   name: string;
   age: number;
