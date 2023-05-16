5b9350033e961bac07b1f0a7a3a7522eb88d52c8
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-16 18:16:58 +0600

    union-intersection.ts commented all code

--- module-4_5-conceptual-session/src/union-intersection.ts
+++ module-4_5-conceptual-session/src/union-intersection.ts
@@ -1,21 +1,21 @@
-// Intersection Type
-type HeroA = {
-  name: string;
-};
-type HeroB = HeroA & {
-  age: number;
-  position: string;
-};
+// // Intersection Type
+// type HeroA = {
+//   name: string;
+// };
+// type HeroB = HeroA & {
+//   age: number;
+//   position: string;
+// };
 
-const heroData: HeroB = {
-  name: "Abul",
-  age: 52,
-  position: "Developer",
-};
+// const heroData: HeroB = {
+//   name: "Abul",
+//   age: 52,
+//   position: "Developer",
+// };
 
-// Union Type
-const myFavHero: HeroA | HeroB = {
-  name: "Abul",
-  //   age: 52,
-  position: "Developer",
-};
+// // Union Type
+// const myFavHero: HeroA | HeroB = {
+//   name: "Abul",
+//   //   age: 52,
+//   position: "Developer",
+// };
