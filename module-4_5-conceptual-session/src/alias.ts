b958de336a92b9fb152f6a9a89688f1ab3f9d662
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-16 12:11:21 +0600

    code increment with static type

--- module-4_5-conceptual-session/src/alias.ts
+++ module-4_5-conceptual-session/src/alias.ts
@@ -2,3 +2,27 @@ type HereIncomeType = number | string;
 
 const hereIncome1: HereIncomeType = 15;
 const hereIncome2: HereIncomeType = "15";
+
+const here1: {
+  name: string;
+  age: number;
+} = {
+  name: "Abul",
+  age: 55,
+};
+
+const here2: {
+  name: string;
+  age: number;
+} = {
+  name: "Abul",
+  age: 55,
+};
+
+const here3: {
+  name: string;
+  age: number;
+} = {
+  name: "Abul",
+  age: 55,
+};
