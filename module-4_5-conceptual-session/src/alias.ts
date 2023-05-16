0e41de0e08cb4e73d57d326218a753760e8b4bad
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-16 12:16:05 +0600

    code increment with alias  with interface

--- module-4_5-conceptual-session/src/alias.ts
+++ module-4_5-conceptual-session/src/alias.ts
@@ -3,21 +3,21 @@ type HereIncomeType = number | string;
 const hereIncome1: HereIncomeType = 15;
 const hereIncome2: HereIncomeType = "15";
 
-type HeroType = {
+interface IHeroType {
   name: string;
   age: number;
-};
-const here1: HeroType = {
+}
+const here1: IHeroType = {
   name: "Abul",
   age: 55,
 };
 
-const here2: HeroType = {
+const here2: IHeroType = {
   name: "Abul",
   age: 55,
 };
 
-const here3: HeroType = {
+const here3: IHeroType = {
   name: "Abul",
   age: 55,
 };
