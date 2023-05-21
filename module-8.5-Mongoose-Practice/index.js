ffb1f84891237e0f738ef33a25e994017a5c2992
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-20 20:09:04 -0700

    practice problem 2 solved

--- module-8.5-Mongoose-Practice/index.js
+++ module-8.5-Mongoose-Practice/index.js
@@ -1,2 +1,8 @@
 // Find all users who are located in New York.
 db.practiceModuleData.aggregate([{ $match: { "address.city": "New York" } }]);
+
+// Find the user(s) with the email "johndoe@example.com" and retrieve their favorite movie.
+db.practiceModuleData.aggregate([
+  { $match: { email: "johndoe@example.com" } },
+  { $project: { "favorites.movie": 1 } },
+]);
