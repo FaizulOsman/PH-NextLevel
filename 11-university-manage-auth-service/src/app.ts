18bc82a907fbfc99d32149696c226362964e6020
FaizulOsman <faizulosmanrabby@gmail.com>
2023-05-29 15:59:33 +0600

    feat: express, cors setup done

--- 11-university-manage-auth-service/src/app.ts
+++ 11-university-manage-auth-service/src/app.ts
@@ -1,7 +1,14 @@
-import express from "express";
-const app = express();
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
-app.get("/", (req, res) => {
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
 
